607f1b384a039bf035721efa4aec1f0b8fc0bccc
Andrea <andrea.fronzi@studio.unibo.it>
2024-11-02 19:01:59 +0100

    add program that implement some operation with list and map to better understand how them work

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -2,12 +2,21 @@ package it.unibo.collections;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int INITIAL_VALUE = 1000;
+    private static final int FINAL_VALUE = 2000;
+    private static final int ELEM = 100_000;
 
     private UseListsAndMaps() {
     }
@@ -21,19 +30,34 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        final List<Integer> list = new ArrayList<>();
+
+        for(int i = INITIAL_VALUE; i < FINAL_VALUE; i++){
+            list.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        final List<Integer> list2 = new LinkedList<>(list);
+
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        Integer tmp = list.getFirst();
+        list.removeFirst();
+        list.addFirst(list.getLast());
+        list.removeLast();
+        list.addLast(tmp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for(final Integer e: list){
+            System.out.println(e);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +65,75 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long time = System.nanoTime();
+        for(int i = 0; i < ELEM; i++){
+            list.addFirst(Integer.valueOf(i));
+        }
+        time = System.nanoTime() - time;
+        var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Converting "
+                + list.size()
+                + " ints to String and inserting them in a Set took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
+
+        time = System.nanoTime();
+        for(int i = 0; i < ELEM; i++){
+            list2.addFirst(Integer.valueOf(i));
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Converting "
+                + list2.size()
+                + " ints to String and inserting them in a Set took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        final int middleElement = list.size() / 2;
+        time = System.nanoTime();
+        for(int i = 0; i < INITIAL_VALUE; i++){
+            list.get(middleElement);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Converting "
+                + list.size()
+                + " ints to String and inserting them in a Set took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
+
+        time = System.nanoTime();
+        for(int i = 0; i < INITIAL_VALUE; i++){
+            list2.get(middleElement);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            "Converting "
+                + list2.size()
+                + " ints to String and inserting them in a Set took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -66,5 +153,18 @@ public final class UseListsAndMaps {
         /*
          * 8) Compute the population of the world
          */
+        final Map<String, Long> map = new HashMap<>();
+        map.put("Africa", 1_110_635_000L);
+        map.put("Americas", 972_005_000L);
+        map.put("Antarctica", 0L);
+        map.put("Asia", 4_298_723_000L);
+        map.put("Europe", 742_452_000L);
+        map.put("Oceania", 38_304_000L);
+
+        final var continents = map.entrySet();
+        for(final var e : continents){
+            System.out.println(e);
+        }
+        
     }
 }
